caeb5c4fa7e6a908ce30d4cc160b71ac52531b66
nate-d-olson <nathandavidolson@gmail.com>
2017-03-14 13:01:54 -0400

    uncommented lines

--- code/qiime_pipeline.sh
+++ code/qiime_pipeline.sh
@@ -3,13 +3,13 @@
 ## Based on the Illumina Tutorial http://qiime.org/1.6.0/tutorials/illumina_overview_tutorial.html
 
 #multiple_join_paired_ends.py -i ../data/ -o joined_pairs
-#rm joined_pairs/*/*un*
+rm joined_pairs/*/*un*
 
 #removed unpaired reads - 
-#multiple_split_libraries_fastq.py -i joined_pairs -o split_libs --read_indicator fastqjoin.join --demultiplexing_method sampleid_by_file --include_input_dir_path --remove_filepath_in_name
+multiple_split_libraries_fastq.py -i joined_pairs -o split_libs --read_indicator fastqjoin.join --demultiplexing_method sampleid_by_file --include_input_dir_path --remove_filepath_in_name
 
 ## Chimera checking
-#identify_chimeric_seqs.py -i split_libs/seqs.fna -o split_libs_chimera -m usearch61 --threads 6 --suppress_usearch61_ref
+identify_chimeric_seqs.py -i split_libs/seqs.fna -o split_libs_chimera -m usearch61 --threads 6 --suppress_usearch61_ref
 
 # Clustering
 ## Open Reference clustering
