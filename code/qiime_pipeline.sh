ea02682af9e4b88dc469f403dda527f3e9ac931c
nate-d-olson <nathandavidolson@gmail.com>
2017-03-15 16:53:40 -0400

    removed chimera filtering from pipeline

--- code/qiime_pipeline.sh
+++ code/qiime_pipeline.sh
@@ -5,12 +5,10 @@
 ## Merge forward and reverse reads
 multiple_join_paired_ends.py -i ../data/ -o joined_pairs
 rm joined_pairs/*/*un*
-multiple_split_libraries_fastq.py -i joined_pairs -o split_libs --read_indicator fastqjoin.join --demultiplexing_method sampleid_by_file --include_input_dir_path --remove_filepath_in_name
-
-## Chimera checking
-identify_chimeric_seqs.py -i split_libs/seqs.fna -o split_libs_chimera -m usearch61 --threads 6 --suppress_usearch61_ref
+multiple_split_libraries_fastq.py -i joined_pairs -o split_libs --read_indicator fastqjoin.join \
+	--demultiplexing_method sampleid_by_file --include_input_dir_path --remove_filepath_in_name
 
 # Clustering
 ## Open Reference clustering
-pick_open_reference_otus.py -o otus_uc_fast_no_chimera/ -i split_libs_chimera/seqs.fna_consensus_fixed.fasta -p ../code/qiime_params.txt
+pick_open_reference_otus.py -o otus_uc_fast/ -i split_libs/seqs.fna -p ../code/qiime_params.txt
 
